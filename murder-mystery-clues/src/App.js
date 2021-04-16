24831bf12f8b370d32a6f37b109d1a0473487a30
rebecca-millerr <me@beccamiller.com>
2021-04-16 14:30:43 -0400

    semaphore, instagram, twitter

--- murder-mystery-clues/src/App.js
+++ murder-mystery-clues/src/App.js
@@ -1,20 +1,26 @@
 import Animals from './clues/Animals';
+import Header from './clues/Header';
 import SocialMedia from './clues/SocialMedia';
 import Diary from './clues/Diary';
 import Morse from './clues/Morse';
+import Semaphore from './clues/Semaphore';
 import Divider from './components/Divider';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
+      <Header />
+      <Divider />
       <SocialMedia />
+      <Divider />
+      <Morse />
       <Divider />
       <Animals />
       <Divider />
       <Diary />
       <Divider />
-      <Morse />
+      <Semaphore />
     </div>
   );
 }
--- murder-mystery-clues/src/clues/SocialMedia.js
+++ murder-mystery-clues/src/clues/SocialMedia.js
@@ -6,6 +6,8 @@ const SocialMedia = () => {
         <section className="social-media">
             <p><a href="https://www.linkedin.com/in/spielberg-mysterio/">LinkedIn</a></p>
             <p><a href="https://www.facebook.com/spielberg.mysterio/">Facebook</a></p>
+            <p><a href="https://www.instagram.com/spielberg.mysterio/">Instagram</a></p>
+            <p><a href="https://twitter.com/SpielbergMyste1">Twitter</a></p>
         </section>
     )
 }
